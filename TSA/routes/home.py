7106318a1810f834c792acc74cc9decfa185d5f4
DubiskiTSA <ajanakufaith0@gmail.com>
2019-02-24 00:13:43 -0600

    meetthe team

--- TSA/routes/home.py
+++ TSA/routes/home.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 home_bp = Blueprint("home_bp", __name__, static_folder="static", template_folder="templates")
 
-@home_bp.route("/")
-def hoem():
+@home_bp.route("/home")
+def home():
     return render_template("home.html")
