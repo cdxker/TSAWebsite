7d99107d57f0d9cb8461ad8d73a119949c6fdf1a
DubiskiTSA <ajanakufaith0@gmail.com>
2019-02-24 00:13:56 -0600

    meet the team

--- TSA/routes/__init__.py
+++ TSA/routes/__init__.py
@@ -1,7 +1,9 @@
 from .home import home_bp
 from .pathways import pathway_bp
+from .meet the team import meet the team_bp
 from flask import Flask
 
 def init_app(app:Flask):
+    app.register_blueprint(meet the team_bp)
     app.register_blueprint(pathway_bp)
     app.register_blueprint(home_bp)
